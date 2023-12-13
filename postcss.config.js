6e534f49998a8b6c50e67c6b1e70aa6be0fcd1e0
rishabh491 <rishabhsahani491@gmail.com>
2023-12-13 13:52:20 +0530

    Use State

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,3 @@ module.exports = {
     autoprefixer: {},
   },
 }
-
-<li key={a.id} class="list-group-item">{a.text} {!edit?<><button type="button" class="btn btn-primary btn-sm "  onClick={()=>handleEdit(a.text)}>Edit</button></>:<> <button type="button" class="btn btn-primary btn-sm "  onClick={()=>handleEdit(a.text)}>Save</button></>} 
-<button type="button" class="btn btn-danger btn-sm">Delete</button></li> 
